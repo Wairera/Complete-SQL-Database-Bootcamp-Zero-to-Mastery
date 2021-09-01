0674cb93ba37d69034834854c54c4408988f993b
Philip Wairera <89575335+Wairera@users.noreply.github.com>
2021-09-01 22:16:17 +0300

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,13 @@
 
 /*
 * Write your query here
-*/
+*/SELECT firstname,lastname,age,income
+from "public"."customers"
+where age between 30 and 50 and income <=50000
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
-*/
+*/SELECT avg(income)
+from "public"."customers"
+where age between 20 and 50
